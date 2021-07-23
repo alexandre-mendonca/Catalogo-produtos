5e4a40170dbd3421e26421722326029272bd5aa3
alexandre.rodrigo <alexandre.rodrigo@totvs.com.br>
2021-07-23 18:13:33 -0300

    Commit Produto

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7d/c09d0dd0eaeb001b19abc2585233a69c
@@ -0,0 +1,15 @@
+package br.com.compasso.uol.productsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.compasso.uol.products.ProductsMsApplication;
+
+@SpringBootTest(classes=ProductsMsApplication.class)
+public class ProductsMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ products-ms/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO PRODUTOS(name, description, price) VALUES('Sabonete', 'Sabonete Líquido', 5.65);
+INSERT INTO PRODUTOS(name, description, price) VALUES('Arroz', 'Arroz Tipo 1', 25.00);
+INSERT INTO PRODUTOS(name, description, price) VALUES('Amaciante', 'Amaciante de Roupa', 12.65);
+INSERT INTO PRODUTOS(name, description, price) VALUES('Leite', 'Leite Integral', 3.60);
+INSERT INTO PRODUTOS(name, description, price) VALUES('Bolacha', 'Bolacha Integral', 4.70);
+INSERT INTO PRODUTOS(name, description, price) VALUES('Farinha', 'Farinha de Milho', 6.80);
+INSERT INTO PRODUTOS(name, description, price) VALUES('Sal', 'Sal Iodado', 2.90);
